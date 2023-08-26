03d09092ad3fcd1d63d303c56da9c5e9d01c2c33
GiddyLesGid <gideonlangat@gmail.com>
2023-08-26 02:54:13 -0700

    basics py

---
+++ add2num.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    sum_result = a + b
+    return sum_result
+
+a = 40
+b = 30
+result = add(a, b)
+print(result)
---
+++ while.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+x=3
+while x<10:
+    print(x)
+    x+=1
