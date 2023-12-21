ea3cd5bb40d7bd289a8d352a801dd2c337e26cd7
samuelsenerwa <samuelsenerwa@gmail.com>
2023-12-21 16:34:01 +0300

    added python code for various shapes

---
+++ interview_prep/shapes/square.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+# a program to print out a square
+for i in range(6):
+    print("*" *6)
---
+++ interview_prep/shapes/triangle.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+# A program to print a triangle
+
+for i in range(4):
+    print('*' *(i+1))
