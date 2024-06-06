1e2f4608f283845a3f2faaefd93555df7315a3a1
samuelsenerwa <samuelsenerwa@gmail.com>
2024-06-06 11:14:13 +0300

    feat:added ASCII & last digit quiz

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+The program that assigns a random signed number to a variable number
+each time it is executed
+"""
+
+import random
+
+number = random.randint(-1000, 1000)
+last_digit = abs(number) % 10 if number >= 0 else -(abs(number) % 10)
+
+print(f"Last digit of {number} is {last_digit}", end="")
+
+if last_digit > 5:
+    print(" and is greater than 5")
+elif last_digit == 0:
+    print(" and is 0")
+else:
+    print(" and is less than 6 and not 0")
---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""
+A program that prints the ASCII alphabet, in lowercase, not followed by a new line
+"""
+
+for i in range(97, 123):
+    print(chr(i), end="")
