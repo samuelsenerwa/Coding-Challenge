0ad77ea291f27a63316307240e753ce48d3ff891
samuelsenerwa <samuelsenerwa@gmail.com>
2024-06-06 12:22:17 +0300

    feat:fizzbuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+A function that prints the number from 1 to 100 seperated by a space
+"""
+
+
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", end=" ")
+        elif i % 3 == 0:
+            print("Fizz", end=" ")
+        elif i % 5 == 0:
+            print("Buzz", end=" ")
+        else:
+            print("{:d}".format(i), end=" ")
