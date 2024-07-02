611b3e6fcae24d2e6c39c873aea9b23451a624ac
samuelsenerwa <samuelsenerwa@gmail.com>
2024-07-02 10:31:47 +0300

    feat: did maximal square leetcode

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+Prototype: def square_matrix_simple(matrix=[])
+matrix is a 2 dimensional array
+
+Returns a new matrix:
+◦ Same size as matrix
+◦ Each value should be the square of the value of the input
+"""
+
+
+def square_matrix_simple(matrix=[]):
+    lst = []
