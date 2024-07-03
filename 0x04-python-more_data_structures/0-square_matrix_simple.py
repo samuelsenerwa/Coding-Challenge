5317d6083c79690d3c6bcd8ed02a7802f0e854b6
samuelsenerwa <samuelsenerwa@gmail.com>
2024-07-03 12:08:43 +0300

    feat:added square matrix problem

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -11,4 +11,9 @@ Returns a new matrix:
 
 
 def square_matrix_simple(matrix=[]):
-    lst = []
+    new_matrix = matrix.copy()
+
+    for i in range(len(matrix)):
+        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
+
+    return new_matrix
