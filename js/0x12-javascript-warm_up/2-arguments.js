bf173153d6d5c6ac493c79bee9a377fc69699755
samuelsenerwa <samuelsenerwa@gmail.com>
2024-07-30 23:23:32 +0300

    did simple javascript task

---
+++ js/0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+// a script that prints a message depending of the number of arguments passed
+
+const args = process.argv.slice[2]
+
+if(args.length === 0){
+    console.log('No argument was found');
+}
+
+else if (args.length === 1){
+    console.log('Argument found')
+}
+
+else {
+    console.log('Arguments found');
+}
