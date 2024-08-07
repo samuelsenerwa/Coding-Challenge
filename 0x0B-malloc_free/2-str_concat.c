38856f12e77f7668b1a1d33189234d8904834595
samuelsenerwa <samuelsenerwa@gmail.com>
2024-08-07 09:33:42 +0300

    added a new main file for string concatenation

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ *
+ * @brief: a function that concatenates two strings.
+ *
+ * function: char *str_concat(char *s1, char *s2)
+ *
+ * returned pointer should point a newly allocated memory  which contains
+ * contents of s1
+ *
+ * on failure: return NULL
+ *
+ */
+
+char *str_concat(char *s1, char *s2){
+
+}
