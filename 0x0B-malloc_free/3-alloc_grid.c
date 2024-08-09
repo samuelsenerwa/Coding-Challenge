033703beef0991b3613ed243de796696ffea6726
samuelsenerwa <samuelsenerwa@gmail.com>
2024-08-09 23:12:43 +0300

    initialized the alloc_grid file

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * @brief a function that returns a pointer to a 2 dimensional array of integers
+ *
+ * @param int **alloc_grid(int width, int height)
+ *
+ * @return return NULL otherwise If width or height is 0 or negative, return NULL
+ *
+ */
